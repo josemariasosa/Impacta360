1b6e89639d0d762c9241eb30497528986b0545f9
josemariasosa <jose.skedar@gmail.com>
2024-09-12 18:00:44 -0600

    yep

--- packages/hardhat/contracts/YourContract.sol
+++ packages/hardhat/contracts/YourContract.sol
@@ -1,87 +1,110 @@
-//SPDX-License-Identifier: MIT
-pragma solidity >=0.8.0 <0.9.0;
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity 0.8.24;
 
-// Useful for debugging. Remove when deploying to a live network.
-import "hardhat/console.sol";
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
+import {IERC20} from "@openzeppelin/contracts/interfaces/IERC20.sol";
+import {SafeERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 
-// Use openzeppelin to inherit battle-tested implementations (ERC20, ERC721, etc)
-// import "@openzeppelin/contracts/access/Ownable.sol";
+// Uncomment this line to use console.log
+// import "hardhat/console.sol";
 
-/**
- * A smart contract that allows changing a state variable of the contract and tracking the changes
- * It also allows the owner to withdraw the Ether in the contract
- * @author BuidlGuidl
- */
-contract YourContract {
-	// State Variables
-	address public immutable owner;
-	string public greeting = "Building Unstoppable Apps!!!";
-	bool public premium = false;
-	uint256 public totalCounter = 0;
-	mapping(address => uint) public userGreetingCounter;
+struct Project {
+    address owner;
+    string description;
+    uint16 category;
+	string[] goals;
+}
 
-	// Events: a way to emit log statements from smart contract that can be listened to by external parties
-	event GreetingChange(
-		address indexed greetingSetter,
-		string newGreeting,
-		bool premium,
-		uint256 value
-	);
+contract Curator {
+	mapping(address => bool) internal _curators;
 
-	// Constructor: Called once on contract deployment
-	// Check packages/hardhat/deploy/00_deploy_your_contract.ts
-	constructor(address _owner) {
-		owner = _owner;
-	}
-
-	// Modifier: used to define a set of rules that must be met before or after a function is executed
-	// Check the withdraw() function
-	modifier isOwner() {
-		// msg.sender: predefined variable that represents address of the account that called the current function
-		require(msg.sender == owner, "Not the Owner");
+	modifier onlyCurator {
+		require(_curators[msg.sender], "Invalid Curator");
 		_;
 	}
 
-	/**
-	 * Function that allows anyone to change the state variable "greeting" of the contract and increase the counters
-	 *
-	 * @param _newGreeting (string memory) - new greeting to save on the contract
-	 */
-	function setGreeting(string memory _newGreeting) public payable {
-		// Print data to the hardhat chain console. Remove when deploying to a live network.
-		console.log(
-			"Setting new greeting '%s' from %s",
-			_newGreeting,
-			msg.sender
-		);
-
-		// Change state variables
-		greeting = _newGreeting;
-		totalCounter += 1;
-		userGreetingCounter[msg.sender] += 1;
-
-		// msg.value: built-in global variable that represents the amount of ether sent with the transaction
-		if (msg.value > 0) {
-			premium = true;
-		} else {
-			premium = false;
-		}
-
-		// emit: keyword used to trigger an event
-		emit GreetingChange(msg.sender, _newGreeting, msg.value > 0, msg.value);
+	function isCurator(address _account) external returns (bool) {
+		return _curators[_account];
 	}
 
-	/**
-	 * Function that allows the owner to withdraw all the Ether in the contract
-	 * The function can only be called by the owner of the contract as defined by the isOwner modifier
-	 */
-	function withdraw() public isOwner {
-		(bool success, ) = owner.call{ value: address(this).balance }("");
-		require(success, "Failed to send Ether");
-	}
+	function _addCurator(_address)
+}
 
-	/**
-	 * Function that allows the contract to receive ETH
-	 */
-	receive() external payable {}
+contract Impacta360 is Ownable {
+
+    using SafeERC20 for IERC20;
+
+    /// donationCategory
+    /// 0 - environment
+    /// 1 - education
+    /// 2 - social
+
+	address public 
+
+    uint16 constant public MAX_DONATION_CATEGORY = 3;
+
+    uint256 private _lastProjectId;
+
+    /// projectId => Project
+    mapping(uint256 => Project) public projects;
+
+    /// projectId => token => amount
+    mapping(uint256 => mapping(address => uint256)) public balances;
+
+    /// token => 
+    mapping(address => bool) public validTokens;
+
+    event Donation(address _donator, IERC20 _token, uint256 _amount);
+    event DonationInvoice(address _donator, IERC20 _token, uint256 _amount, string _invoiceData);
+
+    constructor(address _owner) Ownable(_owner) {}
+
+    function addValidToken(IERC20 _token) external onlyOwner {
+        validTokens[address(_token)] = true;
+    }
+
+    function removeValidToken(IERC20 _token) external onlyOwner {
+        validTokens[address(_token)] = false;
+    }
+
+    function donate(IERC20 _token, uint256 _amount, uint256 _projectId) external {
+        _isValidDonation(_token);
+
+        _token.safeTransferFrom(msg.sender, address(this), _amount);
+        balances[_projectId][address(_token)] += _amount;
+
+        emit Donation(msg.sender, _token, _amount);
+    }
+
+    function donateEnteprise(
+        IERC20 _token,
+        uint256 _amount,
+        uint256 _projectId,
+        string memory _invoiceData
+
+    ) external {
+        _isValidDonation(_token);
+
+        _token.safeTransferFrom(msg.sender, address(this), _amount);
+        balances[_projectId][address(_token)] += _amount;
+
+        emit DonationInvoice(msg.sender, _token, _amount, _invoiceData);
+    }
+
+    function _isValidDonation(IERC20 _token) private view {
+        require(validTokens[address(_token)], "Invalid token");
+    }
+
+    function _inArray(uint256 _amount, uint256[4] memory _array) private pure returns (bool) {
+        for (uint i; i < 4; i++) {
+            if (_array[i] == _amount) return true;
+        }
+        return false;
+    }
+
+    function createProject(address _owner, string memory _description, uint16 _category) external {
+        uint _id = _lastProjectId++;
+        projects[_id] = Project(_owner, _description, _category);
+    }
+
 }
